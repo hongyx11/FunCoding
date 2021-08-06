4e0f8285b994cbaa79947407156211b595812035
yuxi <yuxi.hong@kaust.edu.sa>
2021-08-06 13:44:11 +0300

    update structs of folder

---
+++ leetcode/MonotonicQueue/239.cpp
@@ -0,0 +1,11 @@
+#include <header.h>
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        
+    }
+};
+
+int main(){
+
+}
---
+++ leetcode/MonotonicQueue/862.cpp
@@ -0,0 +1,15 @@
+#include <header.h>
+
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        
+    }
+};
+
+
+int main() {
+    
+    
+}
