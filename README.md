b20852d2328d757079c53c25e27884fb9b88d8b5
yuxi <yuxi.hong@kaust.edu.sa>
2021-08-06 14:42:10 +0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # FunCoding
 Leetcode, Codeforces, etc.
 
-It records my coding on several platform.
+It records my coding on several platforms.
+
+    $mkdir build
+    $cd build && cmake -DCMAKE_INSTALL_PREFIX=$(pwd) ..
+    $make 
+
+Good to GO!
--- header.h
+++ header.h
@@ -1,18 +1,13 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <memory.h>
-#include <stdbool.h>
-
 #include <vector>
 #include <string>
 #include <unordered_map>
 #include <set>
 #include <iostream>
-using namespace std;
 
+using namespace std;
 
 
 
